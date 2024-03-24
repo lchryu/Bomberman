6906f95a457b6b6ff77bf18cb88798c6d2372d6f
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2024-03-24 21:18:53 +0700

    Sprite Animation

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,32 +5,43 @@ public class MovementController : MonoBehaviour
     public new Rigidbody2D rigidbody { get; set; }
     private Vector2 direction = Vector2.down;
     public float speed = 5f;
-    public KeyCode inputUp = KeyCode.W;
-    public KeyCode inputDown = KeyCode.S;
-    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputUp    = KeyCode.W;
+    public KeyCode inputDown  = KeyCode.S;
+    public KeyCode inputLeft  = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
+
+
+    public AnimatedSpriterenderer spriteRendererUp;
+    public AnimatedSpriterenderer spriteRendererDown;
+    public AnimatedSpriterenderer spriteRendererLeft;
+    public AnimatedSpriterenderer spriteRendererRigt;
+    private AnimatedSpriterenderer activeSpriteRenderer;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
     }
     private void Update()
     {
         if (Input.GetKey(inputUp))
         {
-            SetDirection(Vector2.up);
-        } else if (Input.GetKey(inputDown))
+            SetDirection(Vector2.up, spriteRendererUp);
+        } 
+        else if (Input.GetKey(inputDown))
         {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
         }
         else if(Input.GetKey(inputLeft))
         {
-            SetDirection(Vector2.left);
-        } else if (Input.GetKey(inputRight)) {
-            SetDirection(Vector2.right);
-        } else
+            SetDirection(Vector2.left, spriteRendererLeft);
+        } 
+        else if (Input.GetKey(inputRight)) {
+            SetDirection(Vector2.right, spriteRendererRigt);
+        }
+        else
         {
-            SetDirection(Vector2.zero);
+            SetDirection(Vector2.zero, activeSpriteRenderer);
         }
     }
 
@@ -42,8 +53,16 @@ public class MovementController : MonoBehaviour
         rigidbody.MovePosition(position + translation);
     }
 
-    private void SetDirection(Vector2 newDirection)
+    private void SetDirection(Vector2 newDirection, AnimatedSpriterenderer spriteRenderer)
     {
         direction = newDirection;
+
+        spriteRendererUp.enabled   = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRigt.enabled = spriteRenderer == spriteRendererRigt;
+
+        activeSpriteRenderer = spriteRenderer;
+        spriteRenderer.idle = direction == Vector2.zero;
     }
 }
